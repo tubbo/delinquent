f06584c9fd4c54250b183854f1bea8d785d7fee1
Tom Scott <tscott@weblinc.com>
2019-02-07 14:08:35 -0500

    Fix tests

--- index.test.js
+++ index.test.js
@@ -1,8 +1,9 @@
 const delinquent = require('./index');
 
 test('opacity', () => {
-  document.body.innerHTML = '<h1 id="title">welcome to nginx</h1>';
   delinquent(new Date('2017-02-27'));
 
-  expect(document.getElementById('title').style.opacity).not.toBe(100);
+  const body = document.getElementsByTagName("BODY")[0];
+
+  expect(body.style.opacity).toBe('0');
 });
